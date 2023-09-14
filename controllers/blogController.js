5656237980bdd4733e09062d9bf4b97d41c0a208
qianliu <liu.qiannfu@gmail.com>
2023-09-13 23:40:41 -0400

    blogController

--- controllers/blogController.js
+++ controllers/blogController.js
@@ -3,3 +3,76 @@ const Blog = require('../models/Blog')
 const verifyToken = require('../middlewares/')
 // const bcrypt = require('bcrypt') //hasing password
 
+
+blogController.get('/getAll', async(req, res)=>{ 
+    try { 
+        // Attempt to find all blogs in the database
+        const blogs = await Blog.find({}).populate('userId','-password')
+
+        // If successful, return the blogs as a JSON response with a status code of 200 (OK)
+        return res.status(200).json(blogs)
+    } catch (error) { 
+        // If an error occurs during the process, handle it here
+        return res.status(500).json({error: error.message })
+    } 
+})
+
+blogController.get('/find/:id', async(req,res)=>{
+    try {
+        const blog = await Blog.findById(req.params.id).populate('userId','-password')
+        blog.views += 1
+        await blog.save()
+        return res.status(200).json(blog)
+    } catch (error) {
+        return res.status(500).json({error: error.message })
+    }
+})
+
+blogController.get('/featured', async(req, res)=>{
+    try {
+        const blog = await Blog.find({featured:true}).populate('userId','-password').limits(3)
+        return res.status(200).json(blog)
+    } catch (error) {
+        return res.status(500).json({error: error.message })
+    }
+})
+
+blogController.post('/', verifyToken, async(req, res)=>{
+    try {
+        const blog = await Blog.create({...req.body, userId:req.user.id })
+        return res.status(200).json(blog)
+    } catch (error) {
+        return res.status(500).json({error: error.message })
+    }
+})
+
+
+blogController.put('updateBlog/:id', verifyToken, async(req, res)=>{
+    try {
+        const blog = await Blog.findById(req.params.id)
+        if(blog.userId !== req.user.id){
+            throw new Error("Unauthorized: You are only permitted to update your own posts.")
+        }
+        const updateBlog = await Blog.findByIdandUpdate(req.params.id,{$set: req.body})
+        .populate('userId','-password')
+
+        return res.status(200).json(blog)
+    } catch (error) {
+        return res.status(500).json({error: error.message })
+    }
+})
+
+blogController.put('likeBlog/:id', verifyToken, async(req, res)=>{
+    try {
+        const blog = await Blog.findById(req.params.id)
+        if(blog.likes.includes(req.user.id)){
+            blog.likes = blog.likes.filter((userId) => userId !== req.user.id)
+        }
+        const updateBlog = await Blog.findByIdandUpdate(req.params.id,{$set: req.body})
+        .populate('userId','-password')
+
+        return res.status(200).json(blog)
+    } catch (error) {
+        return res.status(500).json({error: error.message })
+    }
+})
--- middlewares/verifyToken.js
+++ middlewares/verifyToken.js
@@ -1,5 +1,16 @@
 const jwt = require('jsonwebtoken');
 
 const verifyToken = (req, res, next)=>{
-    if()
+    if(!req.header.anthrozation) return res.status(403).json({msg:"Not authorized. No token"})
+    if(!req.header.anthrozation && req.headers.authrization.startsWith("Bearer")){
+        const token = req.header.authrization.split(" ")[1] //grab the token
+        jwt.verify(token, process.env.JWT_SECRET, (err,data)=>{
+            if(err) return res.status(403).json({msg:'Wrong or expired token'})
+            else {
+        } req.user = data //an object with the user id as it's only property 
+           next()
+        }) 
+    }
 }
+
+module.exports = verifyToken
